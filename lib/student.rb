b69561ea10415965083cf5f6df959cd2b28a219e
Nicolas Marini <nmarini44@gmail.com>
2018-08-29 20:27:50 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,4 +4,12 @@ class Student
 # DB[:conn]
   attr_accessor :name, :grade
   attr_reader :id
+
+  def initialize(name:, grade:, id: = nil)
+    @name = name
+    @grade = grade
+    @id = id
+  end
+
+
 end
