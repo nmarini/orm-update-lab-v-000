4fa25b330e35c63f81f33c2e4ee232fa57f4abc3
Nicolas Marini <nmarini44@gmail.com>
2018-08-29 20:31:04 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,21 +1,21 @@
 require_relative "../config/environment.rb"
 
 class Student
- 
+
 attr_accessor :name, :grade
 attr_reader :id
 
-  def initialize(name:, grade:, id: = nil)
+  def initialize(name:, grade:, :id = nil)
     @name = name
     @grade = grade
     @id = id
   end
 
   def self.create_table
-    sql = <<-SQL 
+    sql = <<-SQL
       CREATE TABLE students (
         id INTEGER PRIMARY KEY,
-        name TEXT, 
+        name TEXT,
         grade TEXT
       )
       SQL
